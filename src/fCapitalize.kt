259197fcc232bf812c33a7f7b11304afe1b38725
Albert Gilmanov <albertgilmanov8@gmail.com>
2020-09-25 17:09:57 +0500

    stepik3

---
+++ src/fCapitalize.kt
@@ -0,0 +1,14 @@
+import java.util.*
+val scan = Scanner(System.`in`)
+
+fun main(args: Array<String>){
+    // Ваш код здесь.
+    print("hi")
+    myFun()
+}
+
+fun myFun() {
+    TODO("Not yet implemented")
+}
+
+
